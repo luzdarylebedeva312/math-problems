eb5ff10e3edadc11011957376198fdafde44e1f7
luzdarylebedeva312 <luzdarylebedeva312@gmail.com>
2025-03-14 12:52:27 +0000

    Add 9.rs

---
+++ src/9.rs
@@ -0,0 +1,8 @@
+use rand::Rng;
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let num1: i32 = rng.gen_range(1..10);
+    let num2: i32 = rng.gen_range(1..10);
+    println!("{} + {} =", num1, num2);
+}
