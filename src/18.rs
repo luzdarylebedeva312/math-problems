0182c45de10df7995a01d52f1319e421c940d4bc
luzdarylebedeva312 <luzdarylebedeva312@gmail.com>
2025-03-23 18:21:49 +0000

    Add 18.rs

---
+++ src/18.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // Define variables
+    let x = 5;
+    let y = 10;
+
+    // Perform mathematical operation
+    let result = x + y;
+
+    // Print result
+    println!("The sum of {} and {} is: {}", x, y, result);
+}
