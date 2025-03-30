959801e8b6896108c44142a69bf3a0ffdc13d468
luzdarylebedeva312 <luzdarylebedeva312@gmail.com>
2025-03-30 05:15:07 +0000

    Add 21.rs

---
+++ src/21.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // Example: Addition of two numbers
+    let num1 = 5;
+    let num2 = 3;
+    println!("The sum of {} and {} is {}", num1, num2, num1 + num2);
+    
+    // Example: Division with remainder
+    let dividend = 7;
+    let divisor = 2;
+    println!("The result of dividing {} by {} is {}.", dividend, divisor, dividend / divisor);
+}
