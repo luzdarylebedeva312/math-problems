c226a466105c7180a56ba394e88c1435e6fa17b4
luzdarylebedeva312 <luzdarylebedeva312@gmail.com>
2025-03-31 05:21:14 +0000

    Add 22.rs

---
+++ src/22.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // Example of a simple function with one parameter and a conditional expression
+    let age = 20;
+    
+    if age >= 18 {
+        println!("You are an adult.");
+    } else {
+        println!("You are not an adult yet.");
+    }
+}
