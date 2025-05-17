3a4930ff8c347b16fc1da967051ad6ca02ea83db
luzdarylebedeva312 <luzdarylebedeva312@gmail.com>
2025-05-17 03:45:40 +0000

    Add 36.rs

---
+++ src/36.rs
@@ -0,0 +1,21 @@
+// This is just an example of a basic arithmetic problem in Rust.
+fn main() {
+    let num1 = 5;
+    let num2 = 3;
+
+    // Addition
+    let sum = num1 + num2;
+    println!("The sum is: {}", sum);
+
+    // Subtraction
+    let difference = num1 - num2;
+    println!("The difference is: {}", difference);
+
+    // Multiplication
+    let product = num1 * num2;
+    println!("The product is: {}", product);
+
+    // Division
+    let quotient = num1 / num2;
+    println!("The quotient is: {}", quotient);
+}
