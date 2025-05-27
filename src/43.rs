ee497a13b4249160f1277ff7e948f2f877a9b7fa
luzdarylebedeva312 <luzdarylebedeva312@gmail.com>
2025-05-27 17:23:19 +0000

    Add 43.rs

---
+++ src/43.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // This is an example of generating a random string of characters.
+    let random_string = String::from("ABCDEFGHijklMNOPQRSTUVWXYZ");
+
+    println!("Random string: {}", random_string);
+}
